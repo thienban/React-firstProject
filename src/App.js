48f9c808b5aa0eddd24147b81f67e63654db0c5e
thienban <thienban91@hotmail.fr>
2018-03-17 16:58:58 +0100

    props and state

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './Welcome'
+
+class App extends Component {
+  render() {
+    return (
+     <Welcome name="ThienBan"/>
+    );
+  }
+}
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    constructor(){
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+
+    removeOne(){
+        this.setState({
+            count: this.state.count -1
+        });
+    }
+
+    addOne(){
+        this.setState({
+            count: this.state.count +1
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Welcome {this.props.name}</h1>
+                <p>Mon compteur : {this.state.count}</p>
+                <button onClick={() =>this.addOne()}> Ajouter 1</button>
+                <button onClick={() =>this.removeOne()}>Enlever 1</button>
+            </div>
+        );
+    }
+}
+
+export default Welcome;
