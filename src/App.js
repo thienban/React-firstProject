858236faf76a4787e96156dc8481642c933d0828
thienban <thienban91@hotmail.fr>
2018-03-19 19:48:37 +0100

    TodoList component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './Welcome'
 
+import TodoList from './Todo/TodoList'
 class App extends Component {
   render() {
     return (
-     <Welcome name="ThienBan"/>
+     <TodoList />
     );
   }
 }
